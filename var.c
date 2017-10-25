ba4f5baf35515b1b5a218f6db96455be5adea01e
Requiel20 <luca.battistelli@studenti.unicam.it>
2017-10-25 16:18:21 +0100

    more pseudocode, var.neg function

--- var.c
+++ var.c
@@ -24,3 +24,15 @@ bool VARisAssigned(Var v) {
 bool VARisPositive(Var v) {
     return (v->sign);
 }
+
+Var neg(Var p) {
+    bool newSign;
+    if (p->sign == 1) {
+        newSign = 0;
+    } else {
+        newSign = 1;
+    }
+
+    Var output = VARinit(p->id, newSign);
+    return output;
+}
